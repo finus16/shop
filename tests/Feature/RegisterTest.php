b879756618cc2f508a12336a2d4e7c989b4349af
finus16 <m_zaba@wp.pl>
2021-11-09 15:21:46 +0100

    Added register endpoint tests

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Facades\Users;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
@@ -13,6 +15,15 @@ class RegisterTest extends TestCase
     private $name = 'useruser';
     private $email = 'user@user.com';
     private $password = 'qwertyqwerty';
+
+    private $faker;
+
+    protected function setUp(): void
+    {
+        $this->faker = Factory::create();
+
+        parent::setUp();
+    }
 
     /**
      * A basic test example.
@@ -34,5 +45,65 @@ class RegisterTest extends TestCase
         $this->assertDatabaseHas('users', [
             'email' => $this->email
         ]);
+    }
+
+    public function test_register_endpoint()
+    {
+        $response = $this->postJson('/api/users', [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => $password = $this->faker->password,
+            'password_confirmation' => $password
+        ]);
+
+        $response->assertOk();
+        $response->assertSeeText('ok');
+    }
+
+    public function test_register_validation_name()
+    {
+        $response = $this->postJson('/api/users', [
+            'name' => Str::random(1),
+            'email' => $this->faker->safeEmail,
+            'password' => $password = $this->faker->password,
+            'password_confirmation' => $password
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_register_validation_email()
+    {
+        $response = $this->postJson('/api/users', [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmailDomain,
+            'password' => $password = $this->faker->password,
+            'password_confirmation' => $password
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_register_validation_password()
+    {
+        $response = $this->postJson('/api/users', [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => $password = Str::random(5),
+            'password_confirmation' => $password
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_register_validation_need_password_confirm()
+    {
+        $response = $this->postJson('/api/users', [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => $this->faker->password
+        ]);
+
+        $response->assertStatus(422);
     }
 }
